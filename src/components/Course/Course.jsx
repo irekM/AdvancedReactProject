ade523cbe5b034118130e218908babf557e51681
Your Name <you@example.com>
2021-04-06 16:23:27 +0200

    feat. buy user courses

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import bemCssModules from 'bem-css-modules'
+import React, { useContext } from 'react';
+import bemCssModules from 'bem-css-modules';
+import {useHistory} from 'react-router-dom';
 
-import { default as CourseStyles} from './Course.module.scss'
+import request from '../../helpers/request';
+import { default as CourseStyles} from './Course.module.scss';
+import { StoreContext } from '../../store/StoreProvider';
 
 const style = bemCssModules(CourseStyles);
 
-const Course = ({ authors, img, price, title}) =>{
-  const allAuthors = authors.join(', ');
-}
+const Course = ({ authors,id, img,isUserContext = false, price, title}) =>{
+  const {user, setUser} = useContext(StoreContext);
+  const history = useHistory();
 
-const Course = () =>{
-  return(
+
+  const allAuthors = authors.join(', ');
+  const isUserLogged = Boolean(user);
+
+  const handleOnClick = async () =>{
+    try{
+      const {data, status} = await request.patch(
+        '/users',
+        {
+          login: user.login,
+          courseId: id,
+        }
+      );
+
+        if (status === 202){
+          setUser(data.user);
+          history.push('/my-courses');
+
+        }
+
+    } catch (error){
+      console.warn(error);
+    }
+  };
+
+  const shouldBeBuyButtonVisible = isUserLogged && !isUserContext;
+
+  return (
     <li>
     <article className={style()}>
-      <h3 className={style('title')}></h3>
+      <h3 className={style('title')}>{title}</h3>
       <img alt={title} className={style('img')} src ={img}/>
       <p className={style('price')}>{`koszt kursu: ${rice}zł`}</p>
       <p className={style('authors')}>{`Autorzy kursu: ${allAuthors}`}</p>
+      {shouldBeBuyButtonVisible && <button onClick={handleOnClick}>Zakup ten kurs</button>}
     </article>
     </li>
-  )
+  );
 }
 
 export default Course;
